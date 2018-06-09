5aa5a761b2eaecb83d4fcdd79644dc4ba113df3f
Gurwinder <ThisisGurwinder@Gmail.com>
2018-06-09 17:19:25 +0530

    Building a simple prototype of using svg in graphs

--- prototypes/whiteboard/version01/whiteboard/src/App.js
+++ prototypes/whiteboard/version01/whiteboard/src/App.js
@@ -1,34 +1,12 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Graph from "./components/graph.js";
 
-class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <BasicSvg />
-          <BasicSvg />
-        </p>
-      </div>
-    );
+      <Graph data={this.props.data} />
+    )
   }
 }
-
-const BasicSvg = () =>
-  <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-    <circle
-      cx="50"
-      cy="50"
-      r="40"
-      stroke="blue"
-      strokeWidth="4"
-      fill="lightblue"
-    />
-  </svg>;
-
-export default App;
